1dd4585ca254dda176e42a2bfb9528bee1b66154
matt vickers <matt.vickers@sky.com>
2016-10-24 13:52:51 +0100

    Initial Commit

---
+++ Attendr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Attendr
+//
+//  Created by Matt Vickers on 24/10/2016.
+//  Copyright © 2016 Matt Vickers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
